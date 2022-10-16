7fa898cf9cccc3551c9affb6bae67c5a2d12e6d1
Guglielmo Cola <guglielmocola@gmail.com>
2022-10-16 11:14:51 +0200

    New description in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='twittermonitor',
-    description='Library to exploit Twitter API v2 on JupyterHub.',
+    description='Library to ease the use of Twitter API v2 on JupyterHub',
     version='0.1.0',
     packages=[
         "twittermonitor",
